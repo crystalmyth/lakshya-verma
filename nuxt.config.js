1e14591ebfc5167d88aa67123a286038e601170e
crystalmyth <lakshayverma.lv@gmail.com>
2022-08-07 02:24:06 +0530

    change

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   target: 'static',
   head: {
-    title: 'trying',
+    title: 'Lakshya Verma | Portfolio',
     meta: [{
         charset: 'utf-8'
       },

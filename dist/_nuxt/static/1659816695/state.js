dab200ca205418bdce64546b75df25a0e69121f9
crystalmyth <lakshayverma.lv@gmail.com>
2022-08-07 02:03:28 +0530

    change

--- dist/_nuxt/static/1659816695/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Flakshya-verma\u002F_nuxt\u002Fstatic\u002F1659816695",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Flakshya-verma\u002F",assetsPath:"\u002Flakshya-verma\u002F_nuxt\u002F",cdnURL:a}}}}(null));
